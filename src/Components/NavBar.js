9bedc8495c5baf9024ae1d091c073d2467024670
anahinatale <nataleanahi@gmail.com>
2021-09-06 22:16:38 -0300

    NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar () {
+return (
+<div className="NavBar">
+   <a href="#section">Inicio</a>
+   <a href="#section">Quienes somos</a>
+   <a href="#section">Contacto</a>
+   <a href="#section">Carrito de compras</a>
+</div>
+);
+};
+export default NavBar;
